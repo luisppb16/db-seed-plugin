2d944799f196c2c9a13db78cba6e1dc8143c0ba7
Luis Pepe <luispepe0@gmail.com>
2025-11-21 01:30:16 +0100

    Update SchemaDsl.java

--- src/main/java/com/luisppb16/dbseed/schema/SchemaDsl.java
+++ src/main/java/com/luisppb16/dbseed/schema/SchemaDsl.java
@@ -13,64 +13,47 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class SchemaDsl {
 
-  public static Schema schema(Table... tables) {
+  public static Schema schema(final Table... tables) {
     return new Schema(List.of(tables));
   }
 
-  public static Table table(String name, Column... columns) {
+  public static Table table(final String name, final Column... columns) {
     return new Table(name, List.of(columns));
   }
 
-  public static Column column(String name, SqlType type) {
+  public static Column column(final String name, final SqlType type) {
     return new Column(name, type, false, null);
   }
 
-  public static Column pk(String name, SqlType type) {
+  public static Column pk(final String name, final SqlType type) {
     return new Column(name, type, true, null);
   }
 
-  public static Column fk(String name, SqlType type, String refTable, String refColumn) {
+  public static Column fk(final String name, final SqlType type, final String refTable, final String refColumn) {
     return new Column(name, type, false, new ForeignKeyReference(refTable, refColumn));
   }
 
-  public static String toSql(Schema schema) {
+  public static String toSql(final Schema schema) {
     return schema.tables().stream().map(SchemaDsl::tableSql).collect(Collectors.joining());
   }
 
-  private static String tableSql(Table table) {
-    String cols =
-        table.columns().stream().map(SchemaDsl::columnSql).collect(Collectors.joining(",\n"));
-    return "CREATE TABLE ".concat(table.name()).concat(" (\n").concat(cols).concat("\n);\n\n");
+  private static String tableSql(final Table table) {
+    final String cols =
+        table.columns().stream().map(SchemaDsl::columnSql).collect(Collectors.joining(",%n"));
+    return "CREATE TABLE %s (%n%s%n);%n%n".formatted(table.name(), cols);
   }
 
-  private static String columnSql(Column column) {
-    StringBuilder sql =
-        new StringBuilder("  ").append(column.name()).append(' ').append(column.type().toSql());
+  private static String columnSql(final Column column) {
+    final StringBuilder sql = new StringBuilder("  %s %s".formatted(column.name(), column.type().toSql()));
 
     if (column.primaryKey()) {
       sql.append(" PRIMARY KEY");
     }
     if (column.isForeignKey()) {
-      ForeignKeyReference fk = column.foreignKey();
-      sql.append(" REFERENCES ").append(fk.table()).append('(').append(fk.column()).append(')');
+      final ForeignKeyReference fk = column.foreignKey();
+      sql.append(" REFERENCES %s(%s)".formatted(fk.table(), fk.column()));
     }
     return sql.toString();
-  }
-
-  public enum SqlType {
-    INT,
-    VARCHAR,
-    TIMESTAMP,
-    BOOLEAN;
-
-    public String toSql() {
-      return switch (this) {
-        case INT -> "INT";
-        case VARCHAR -> "VARCHAR(255)";
-        case TIMESTAMP -> "TIMESTAMP";
-        case BOOLEAN -> "BOOLEAN";
-      };
-    }
   }
 
   public record Schema(List<Table> tables) {

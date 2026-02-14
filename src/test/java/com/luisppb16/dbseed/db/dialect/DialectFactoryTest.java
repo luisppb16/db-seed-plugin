09aa03b3d82e489327603b5defb0df3a57051efb
Luis Pepe <luispepe0@gmail.com>
2026-02-15 00:01:20 +0100

    Changes

--- src/test/java/com/luisppb16/dbseed/db/dialect/DialectFactoryTest.java
+++ src/test/java/com/luisppb16/dbseed/db/dialect/DialectFactoryTest.java
@@ -22,7 +22,7 @@ class DialectFactoryTest {
 
   // ── Auto-detection from driverClass / urlTemplate ──
 
-  static Stream<Arguments> mysqlCases() {
+  static Stream<Arguments> dialectCases() {
     return Stream.of(
         Arguments.of("mysql", "`", "1", "0"),
         Arguments.of("mariadb", "`", "1", "0"),
@@ -42,67 +42,6 @@ class DialectFactoryTest {
     assertThat(d.quote("col")).startsWith(expectedQuoteStart);
     assertThat(d.formatBoolean(true)).isEqualTo(expectedTrue);
     assertThat(d.formatBoolean(false)).isEqualTo(expectedFalse);
-  }
-
-  static Stream<Arguments> sqlserverCases() {
-    return Stream.of(
-        Arguments.of("com.microsoft.sqlserver.jdbc.SQLServerDriver", ""),
-        Arguments.of("", "jdbc:sqlserver://host"));
-  }
-
-  @ParameterizedTest
-  @MethodSource("sqlserverCases")
-  void resolve_sqlserver_squareBrackets(String driverClass, String url) {
-    DatabaseDialect d = DialectFactory.resolve(driver(driverClass, url));
-    assertThat(d.quote("t")).startsWith("[");
-    assertThat(d.formatBoolean(true)).isEqualTo("1");
-    assertThat(d.beginTransaction()).isEqualTo("BEGIN TRANSACTION;\n");
-  }
-
-  static Stream<Arguments> oracleCases() {
-    return Stream.of(
-        Arguments.of("oracle.jdbc.OracleDriver", ""),
-        Arguments.of("", "jdbc:oracle:thin:@host"));
-  }
-
-  @ParameterizedTest
-  @MethodSource("oracleCases")
-  void resolve_oracle_uppercase(String driverClass, String url) {
-    DatabaseDialect d = DialectFactory.resolve(driver(driverClass, url));
-    assertThat(d.quote("myCol")).isEqualTo("\"MYCOL\"");
-    assertThat(d.formatBoolean(true)).isEqualTo("1");
-  }
-
-  static Stream<Arguments> sqliteCases() {
-    return Stream.of(
-        Arguments.of("org.sqlite.JDBC", ""),
-        Arguments.of("", "jdbc:sqlite:file.db"));
-  }
-
-  @ParameterizedTest
-  @MethodSource("sqliteCases")
-  void resolve_sqlite_detected(String driverClass, String url) {
-    DatabaseDialect d = DialectFactory.resolve(driver(driverClass, url));
-    assertThat(d.formatBoolean(true)).isEqualTo("1");
-    assertThat(d.beginTransaction()).isEqualTo("BEGIN TRANSACTION;\n");
-  }
-
-  static Stream<Arguments> postgresqlCases() {
-    return Stream.of(
-        Arguments.of("org.postgresql.Driver", ""),
-        Arguments.of("", "jdbc:postgresql://host/db"),
-        Arguments.of("", "jdbc:redshift://host/db"),
-        Arguments.of("", "jdbc:cockroach://host/db"),
-        Arguments.of("org.h2.Driver", ""));
-  }
-
-  @ParameterizedTest
-  @MethodSource("postgresqlCases")
-  void resolve_postgresql_doubleQuotes(String driverClass, String url) {
-    DatabaseDialect d = DialectFactory.resolve(driver(driverClass, url));
-    assertThat(d.quote("t")).isEqualTo("\"t\"");
-    assertThat(d.formatBoolean(true)).isEqualTo("TRUE");
-    assertThat(d.beginTransaction()).isEqualTo("BEGIN;\n");
   }
 
   // ── Explicit dialect field takes priority ──
